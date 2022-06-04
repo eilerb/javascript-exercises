02c8f12b8148a62331ffd51732a0d5b6e8dd6149
eilerb <eiler.byberi@gmail.com>
2022-06-04 19:22:04 -0400

    Add solutions to the first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+	let str = '';
+	if (repeats < 0) {
+		return 'ERROR';
+	}
+	for (let i = 0; i < repeats; i++) {
+		str += string;
+	}
+	return str;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, elementToRemove) {
+	for (let i  = 0; i < a.length; i++) {
+		if (a[i] === elementToRemove) {
+			array.splice(i, 1);
+		}
+	}
 };
 
 // Do not edit below this line

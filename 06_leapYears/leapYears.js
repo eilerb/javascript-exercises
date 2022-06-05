8a691da5fd319df327a7f1e9849a006ab33b9b92
eilerb <eiler.byberi@gmail.com>
2022-06-05 00:10:12 -0400

    Improve the solution to exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,5 @@
 const leapYears = function(year) {
-	if (year % 4 === 0) {
-		if (year % 100 === 0 && year % 400 !== 0) {
-			return false;
-		} else if (year % 4 === 0 && year % 100 === 0 &&
-		 year % 400 === 0) {
-			return true;
-		}
-	return true;
-	} else {
-		return false;
-	}
+	return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
